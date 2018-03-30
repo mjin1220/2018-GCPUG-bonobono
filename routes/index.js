a87e008643d8d53275f920f75c2e1b2c7b29b7e9
mjin1220 <myeongjin1220@gmail.com>
2018-03-30 18:06:38 +0000

    intermed

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/queryTest', function(req, res, next){
+  const BigQuery = require('@google-cloud/bigquery');
+  const bigquery = new BigQuery({
+    projectId: 'gcp-hackathon18-icn-2910',
+  });
+
+  const options = {
+    query: `select * from bigquery-public-data.chicago_crime.crime limit 1`,
+    timeoutMs: 10000, // Time out after 10 seconds.
+    useLegacySql: false, // Use standard SQL syntax for queries.
+  };
+
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = './config/GCP_Hackathon_Korea_2910-5e0999b1ab4c.json';
+
+  bigquery
+    .query(options)
+    .then(results => {
+      const rows = results[0];
+      console.log('Rows:');
+      rows.forEach(row => console.log(row));
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
+});
 module.exports = router;
